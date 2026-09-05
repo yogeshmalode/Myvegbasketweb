86530d6eb607b76206d0566052f2916efd9405b0
yogeshmalode <yogeshmalode5@gmail.com>
2026-09-06 00:22:55 +0530

    unwanted file deleted

--- vegbasket/admin/includes/auth.php
+++
@@ -1,8 +0,0 @@
-<?php
-// Include this at the top of every protected admin page.
-if (!isset($pdo)) { require_once __DIR__ . '/../../config.php'; }
-
-if (!is_admin_logged_in()) {
-    header('Location: login.php');
-    exit;
-}
--- vegbasket/admin/logout.php
+++
@@ -1,6 +0,0 @@
-<?php
-require_once __DIR__ . '/../config.php';
-session_unset();
-session_destroy();
-header('Location: login.php');
-exit;
